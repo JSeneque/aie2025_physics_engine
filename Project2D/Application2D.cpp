dadda346c4415e95205fc4bd0a40c41d2b79250f
Van Damage <jeannoelseneque@hotmail.com>
2025-05-10 19:41:42 +0930

    Create a demo pool table

--- Project2D/Application2D.cpp
+++ Project2D/Application2D.cpp
@@ -25,18 +25,18 @@ bool Application2D::startup() {
 	m_font = new aie::Font("./font/consolas.ttf", 32);
 
 	m_physicsScene = new PhysicsScene();
-	m_physicsScene->setGravity(glm::vec2(0.0f, -9.82f));
+	m_physicsScene->setGravity(glm::vec2(0.0f, 0.0f));
 	m_physicsScene->setTimeStep(0.01f);
 
-	Sphere* ball1 = new Sphere(glm::vec2(9, 25), glm::vec2(0,1), 4.0f, 4, glm::vec4(1, 0, 0, 1), 0.8f);
+	/*Sphere* ball1 = new Sphere(glm::vec2(9, 25), glm::vec2(0,1), 4.0f, 4, glm::vec4(1, 0, 0, 1), 0.8f);
 	Sphere* ball2 = new Sphere(glm::vec2(5, 40), glm::vec2(10,1), 4.0f, 4, glm::vec4(0, 1, 0, 1), 0.8f);
 	Plane* plane1 = new Plane(glm::vec2(0, 1), -20, 0.1f);		// Horizontal
 	Plane* plane2 = new Plane(glm::vec2(1, 0), -30, 0.1f);			// Vertical
 	Plane* plane3 = new Plane(glm::vec2(-1, 0), -30, 0.1f);	
 	Box* box1 = new Box(glm::vec2(0, -7), glm::vec2(0, 1), 4.0f, glm::vec2(8, 3), glm::vec4(0.5f, 0.5f, 0.5f, 1), 0, 0.8f);
-	Box* box2 = new Box(glm::vec2(-7, 20), glm::vec2(0, 1), 4.0f, glm::vec2(4, 4), glm::vec4(1, 1, 0, 1), 0, 0.8f);
+	Box* box2 = new Box(glm::vec2(-7, 20), glm::vec2(0, 1), 4.0f, glm::vec2(4, 4), glm::vec4(1, 1, 0, 1), 0, 0.8f);*/
 
-	box1->SetKinematic(true);
+	/*box1->SetKinematic(true);
 
 	ball1->SetLinearDrag(0.3f);
 	ball2->SetLinearDrag(0.3f);
@@ -49,8 +49,67 @@ bool Application2D::startup() {
 	m_physicsScene->addActor(plane2);
 	m_physicsScene->addActor(plane3);
 	m_physicsScene->addActor(box1);
-	m_physicsScene->addActor(box2);
+	m_physicsScene->addActor(box2);*/
 
+
+	// balls setup
+	glm::vec2 rackOrigin =  {35.0f, 0.0f};
+
+	float ballRadius = 3.0f;
+	float ballDiameter = ballRadius * 2.0f;
+	float rowSpacing = ballDiameter;						// Horizontal distance between balls in a row
+	float colSpacing = ballDiameter * sqrt(3) / 2.0f;	// Vertical offset (60° triangle)
+	
+	int ballIndex = 0;
+	for (int row = 0; row < 5; ++row)
+	{
+		for (int i = 0; i <= row; ++i)
+		{
+			Sphere* ball = new Sphere({0,0}, {0,0},4,ballRadius, {1,0,0,1}, 1);
+			float x = rackOrigin.x + row * rowSpacing;
+			float y = rackOrigin.y - row * colSpacing / 2.0f + i * colSpacing;
+
+			ball->SetPosition({x,y});
+
+			ball->SetLinearDrag(0.3f);
+			ball->SetAngularDrag(0.3f);
+			
+			//balls.push_back(ball);
+
+			m_physicsScene->addActor(ball);
+		}
+	}
+
+	// table setup
+	Box* topBoundary = new Box(glm::vec2(0, 40), glm::vec2(0, 1), 4.0f, glm::vec2(80, 3), glm::vec4(0.0f, 0.51f, 0.0f, 1), 0, 0.8f);
+	Box* rightBoundary = new Box(glm::vec2(-77, 0), glm::vec2(0, 1), 4.0f, glm::vec2(3, 40), glm::vec4(0.0f, 0.51f, 0.0f, 1), 0, 0.8f);
+	Box* bottomBoundary = new Box(glm::vec2(0, -40), glm::vec2(0, 1), 4.0f, glm::vec2(80, 3), glm::vec4(0.0f, 0.51f, 0.0f, 1), 0, 0.8f);
+	Box* leftBoundary = new Box(glm::vec2(77, 0), glm::vec2(0, 1), 4.0f, glm::vec2(3, 40), glm::vec4(0.0f, 0.51f, 0.0f, 1), 0, 0.8f);
+	
+	topBoundary->SetKinematic(true);
+	rightBoundary->SetKinematic(true);
+	bottomBoundary->SetKinematic(true);
+	leftBoundary->SetKinematic(true);
+		
+	m_physicsScene->addActor(topBoundary);
+	m_physicsScene->addActor(rightBoundary);
+	m_physicsScene->addActor(bottomBoundary);
+	m_physicsScene->addActor(leftBoundary);
+
+	// table pockets
+	Sphere* pocket1 = new Sphere({-80,40}, {0,0},0,ballRadius, {0,0,0,1}, 0);
+	pocket1->SetKinematic(true);
+	//m_physicsScene->addActor(pocket1);
+	
+	// setup cue ball
+
+	Sphere* cueBall = new Sphere({-50,0}, {300,1},4,ballRadius, {1,1,1,1}, 1);
+
+	cueBall->SetLinearDrag(0.3f);
+	cueBall->SetAngularDrag(0.3f);
+	
+	m_physicsScene->addActor(cueBall);
+	
 	return true;
 }
 
